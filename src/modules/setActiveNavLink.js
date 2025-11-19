6741baae222bb8bd71593b3d89c2f11827df6d2a
giang.le2@myy.haaga-helia.fi <giang.le2@myy.haaga-helia.fi>
2025-11-20 00:30:32 +0200

    Navigation panel bugfix

--- src/modules/setActiveNavLink.js
+++ src/modules/setActiveNavLink.js
@@ -3,12 +3,17 @@ export function setActiveNavLink() {
     const navLinks = document.querySelectorAll('.nav-link');
     const currentPath = window.location.pathname.split('/').filter(Boolean);
 
-    if (currentPath.length > 1) {
-        const parentDir = currentPath[1];
+    if (currentPath.length > 0) {
+        let parentDir = currentPath[0];
+
+        // Special case: aloittelijan-opas is part of resources
+        if (parentDir === 'aloittelijan-opas') {
+            parentDir = 'resurssit';
+        }
 
         navLinks.forEach(link => {
             const linkPath = link.getAttribute('href').split('/').filter(Boolean);
-            if (linkPath.length > 1 && linkPath[1] === parentDir) {
+            if (linkPath.length > 0 && linkPath[0] === parentDir) {
                 link.classList.add('active');
             }
         });
